19af6e4d95ee5b6c1c5ef3842be3b8e36ed32c91
damienmillet <contact@damien-millet.dev>
2024-05-27 18:10:49 +0200

    Update include paths for books and livres in API files

--- api/livre.php
+++ api/livre.php
@@ -28,7 +28,7 @@
                             $id = $_GET['id'];
                             // J'inclus le fichier correspondant au livre depuis le dossier livres/
                             var_dump(dirname(__DIR__),$id);
-                            include dirname(__DIR__)."/../../livres/$id.php";
+                            include dirname(__DIR__)."/livres/$id.php";
                         } else {
                             echo "Aucun livre selectionné";
                         }
