213adb48e000d1d54e47ffc5b3760aaf8d3cc809
damienmillet <contact@damien-millet.dev>
2024-05-27 17:45:58 +0200

    Update date display format in script.js

--- public/script.js
+++ public/script.js
@@ -13,8 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
 // update date format with Hhm every 60 seconds
 setInterval(() => {
     const date = new Date();
-    const hours = date("H");
-    const minutes = date("i");
+    const hours = date.getHours().toLocaleString('fr-FR', { minimumIntegerDigits: 2, useGrouping: false });
+    const minutes = date.getMinutes();
     const dateContainer = document.getElementsByClassName('date');
     console.log(dateContainer)
     dateContainer.innerHTML = `${hours}h${minutes}`;
